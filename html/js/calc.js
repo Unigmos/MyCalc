afc542d501df796f942d25ef6ab91d2c48a12cfd
Shaneron <77985354+Unigmos@users.noreply.github.com>
2022-05-06 13:36:22 +0900

    機能追加
    演算機能及びClear機能の実装

--- html/js/calc.js
+++ html/js/calc.js
@@ -1,11 +1,18 @@
 let strs = "";
 
 function set_data(data){
-    var str_data = document.getElementById("str_area")
+    var str_data = document.getElementById("str_area");
     strs += data.value;
     str_data.innerHTML = strs;
 }
 
+function clr_data(){
+    var str_data = document.getElementById("str_area");
+    strs = "";
+    str_data.innerHTML = strs;
+}
+
 function calc(){
-    
+    var calc_data = document.getElementById("str_area");
+    calc_data.innerHTML = eval(strs);
 }
