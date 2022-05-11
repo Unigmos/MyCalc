6a3fce60eeabbe17dcdf5a1c7c1301ed1425e6f9
Shaneron <77985354+Unigmos@users.noreply.github.com>
2022-05-11 11:06:49 +0900

    演算子調整
    連続して演算子が入力できないよう調整

--- html/js/calc.js
+++ html/js/calc.js
@@ -1,9 +1,30 @@
+// 変数定義
 let strs = "";
-let checklist = [];
+let check_list = [];
+let sym_flag = false;
 
 function set_data(data){
     var str_data = document.getElementById("str_area");
-    strs += data.value;
+
+    // id取得→前方一致→フラグ処理
+    if(sym_flag == true){
+        if(data.id.indexOf("sym") == 0){
+            strs = strs.slice(0, -1);
+            strs += data.value;
+            sym_flag = true;
+        }else{
+            strs += data.value;
+            sym_flag = false;
+        }
+    }else{
+        if(data.id.indexOf("sym") == 0){
+            strs += data.value;
+            sym_flag = true;
+        }else{
+            strs += data.value;
+            sym_flag = false;
+        }
+    }
     str_data.innerHTML = strs;
 }
 
